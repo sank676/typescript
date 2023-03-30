f1f6b2fdb7cd5d37edb201ca978621c504a54a22
sank676 <ssankari676@gmail.com>
2023-03-30 16:36:03 +0530

    30-03-2023

---
+++ max.js
@@ -0,0 +1,9 @@
+function findMax(numbers) {
+    var max = numbers[0];
+    for (var i = 1; i < numbers.length; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    return max;
+}
